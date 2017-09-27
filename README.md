f3a7a1456cd55d9e33e8a59869b5976406a41b2b
einstweilenhier <17862290+einstweilenhier@users.noreply.github.com>
2017-09-27 21:59:26 +0000

    Tweaked some values for permanent use

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Requires: lcd4linux>=r1200 with DPF driver, wget, ffmpeg, imagemagick
+run with run.sh or copy lcd4linux.conf to /etc/lcd4linux.conf and use lcd4linux service
--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-lcd4linux -Ff ./lcd4linux.conf -o bild.png &
+#lcd4linux -Ff ./lcd4linux.conf -o bild.png &
+lcd4linux -Ff ./lcd4linux.conf &
 
