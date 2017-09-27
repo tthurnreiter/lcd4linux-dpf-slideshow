38f7ee6c4b92110e19b7e33450f5dffbfb88decc
einstweilenhier <17862290+einstweilenhier@users.noreply.github.com>
2017-09-27 23:13:06 +0200

    Update LCD4Linux, add debug display

--- getimages.sh
+++ getimages.sh
@@ -9,7 +9,7 @@ declare -a static_images=( "http://www.raiba-regensburg.de/webcam/webcam.jpg"
 declare -a mjpeg_streams=(
                    "http://stream.dom.r-kom.de/"
 	           "http://stream.neupfarrplatz.r-kom.de/"
-	           "http://stream.bismarckplatz.r-kom.de/"
+	           #"http://stream.bismarckplatz.r-kom.de/"
         )
 
 counter=0
@@ -17,21 +17,21 @@ filename=image
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-mkdir ./out/
+mkdir -p ./out/
 # Getting static images
 for image in "${static_images[@]}"
 do
     wget -q -O $filename$counter $image
     convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
-    rm ./$filename$counter
+    rm -f ./$filename$counter
     counter=$((counter+1))
 done
 # Getting mjpeg snapshots
 for stream in "${mjpeg_streams[@]}"
 do
-    ffmpeg -i $stream -vframes 1 -r 1 -y -f mjpeg $filename$counter
+    ffmpeg -loglevel warning -i $stream -vframes 1 -r 1 -y -f mjpeg $filename$counter > /dev/null
     convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
-    rm ./$filename$counter
+    rm -f ./$filename$counter
     counter=$((counter+1))
 done
 
