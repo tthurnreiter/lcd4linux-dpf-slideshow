1d29136e5d5fa28369a10c0496cea514e6071171
einstweilenhier <17862290+einstweilenhier@users.noreply.github.com>
2018-03-27 16:07:52 +0000

    First step towards using xscreenshows glslideshow to display the images

--- getimages.sh
+++ getimages.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
-#continue on error
+##continue on error
 set +e
+set -x
 declare -a static_images=( "http://www.raiba-regensburg.de/webcam/webcam.jpg"
   		   "http://www.raiba-regensburg.de/webcam/webcam2.jpg"
-	           "http://195.37.211.74/record/current.jpg"
-	           "http://www.nkk.de/webcam/webcam.jpg"
+		   "http://othcam.oth-regensburg.de/webcam/Regensburg/current/1200.jpg"
+#	           "http://195.37.211.74/record/current.jpg"
+#	           "http://www.nkk.de/webcam/webcam.jpg"
 	)
 declare -a mjpeg_streams=(
                    "http://stream.dom.r-kom.de/"
 	           "http://stream.neupfarrplatz.r-kom.de/"
-	           #"http://stream.bismarckplatz.r-kom.de/"
+#	           "http://stream.bismarckplatz.r-kom.de/"
         )
 
 counter=0
-filename=image
+filename=dlimage
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-mkdir -p ./out/
-# Getting static images
+#mkdir -p ./out/
+
+## delete old images
+rm $filename*
+
+## Getting static images
 for image in "${static_images[@]}"
 do
-    wget -q -O $filename$counter $image
-    convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
-    rm -f ./$filename$counter
+    wget -q -O $filename$counter.jpg $image
+#    convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
+#    rm -f ./$filename$counter
     counter=$((counter+1))
 done
-# Getting mjpeg snapshots
+## Getting mjpeg snapshots
 for stream in "${mjpeg_streams[@]}"
 do
-    ffmpeg -loglevel warning -i $stream -vframes 1 -r 1 -y -f mjpeg $filename$counter > /dev/null
-    convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
-    rm -f ./$filename$counter
+    ffmpeg -loglevel warning -i $stream -vframes 1 -r 1 -y -f mjpeg $filename$counter.jpg > /dev/null
+#    convert $filename$counter -resize '320X240' -background transparent -gravity center -extent '320x240' out/$filename$counter.png
+#    rm -f ./$filename$counter
     counter=$((counter+1))
 done
+
+#rm -r ./out/
 
 exit 0
